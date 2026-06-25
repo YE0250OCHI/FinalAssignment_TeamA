3fac7168a35e24af2cd7d05f21e658f6648a3734
wohara224 <wohara@youtheng.co.jp>
2026-06-25 19:58:44 +0900

    Add PWA

---
+++ src/SimpleAutomaticStorageSystem.Server/wwwroot/js/sw.js
@@ -0,0 +1,20 @@
+﻿self.addEventListener("install", event => {
+    console.log("Service Worker Installed");
+});
+
+self.addEventListener("fetch", event => {
+});
+
+const CACHE_NAME = "app-v1";
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll([
+                "/",
+                "/css/site.css",
+                "/js/site.js"
+            ]);
+        })
+    );
+});
